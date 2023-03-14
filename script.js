07024c6dd268b6bbff68bd05c7200f4c8223609f
chaturvedi-anil <chaturvedia560@gmail.com>
2023-03-14 23:25:53 +0530

    global variable added

---
+++ script.js
@@ -0,0 +1,6 @@
+// all the global const variable 
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
