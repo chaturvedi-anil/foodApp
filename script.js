1589bbea37ed6ccdc8ab267ed11a1fe3328f826f
chaturvedi-anil <chaturvedia560@gmail.com>
2023-03-15 14:27:39 +0530

    meal recipe details api added

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // all global variable
 
-
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('meal-details-content');
@@ -8,6 +7,7 @@ const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 //event Listener
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
 
 //get meal list that mathces with ingredients
 function getMealList()
@@ -45,3 +45,45 @@ function getMealList()
         mealList.innerHTML = html;
     });
 }
+
+// get recipe of that meal 
+
+function getMealRecipe(e)
+{
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn'))
+    {
+        let mealItem = e.target.parentElement.parentElement;
+        // fetch meal details using meal id from the mealdb api 
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+// create Model for recipe 
+
+function mealRecipeModal(meal)
+{
+    meal = meal[0];
+    let html = `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruction">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="">
+        </div>
+
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank" rel="noopener noreferrer">Watch Video</a>
+        </div>
+    `;
+    // console.log(html);
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
+
